79affd513cd4ef227b00dab79bdae27063356197
MingLu1993 <996781107@qq.com>
2017-03-03 19:13:06 +0800

    加入故障诊断类

---
+++ FBGEMSystem/Diagnosis/Diagnosis.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Windows.Forms;
+
+namespace FBGEMSystem.Diagnosis
+{
+    public partial class Diagnosis : Form
+    {
+        public Diagnosis()
+        {
+            InitializeComponent();
+        }
+    }
+}
